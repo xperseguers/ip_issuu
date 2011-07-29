ae5e4fe290cf8bee19f7f37b2107f9e06b27955e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2011-07-29 19:45:04 +0200

    Release Version 0.1.2

--- README.txt
+++ README.txt
@@ -2,7 +2,7 @@ This is just a simple TYPO3 Extension that lets you embed issuu-flip pdfs into y
 
 Warning:
 As of release 0.1.0 this Extension uses Extbase and Fluid as Frameworks. So make sure you are using any TYPO3 Version
-from Version 4.4 and above. This version _may_ break the current configuration.
+from Version 4.5 and above. This version _may_ break the current configuration.
 Simply add the Code new in the backend and everything will be fine again
 
 If you find any errors or want to contribute anything feel free to add a report on the project Website at
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ip_issuu".
 #
-# Auto generated 27-06-2011 12:44
+# Auto generated 29-07-2011 19:44
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.6.99',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"d0eb";s:10:"README.txt";s:4:"d0a1";s:16:"ext_autoload.php";s:4:"c375";s:12:"ext_icon.gif";s:4:"2f2c";s:17:"ext_localconf.php";s:4:"b40c";s:14:"ext_tables.php";s:4:"f707";s:28:"Classes/Controller/Index.php";s:4:"dea1";s:29:"Classes/Domain/Model/Flip.php";s:4:"3637";s:42:"Configuration/Flexform/flexform_ds_pi1.xml";s:4:"50c4";s:44:"Resources/Private/Templates/Index/Index.html";s:4:"f590";s:14:"doc/manual.sxw";s:4:"cc83";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"ef8e";s:10:"README.txt";s:4:"c394";s:16:"ext_autoload.php";s:4:"c375";s:12:"ext_icon.gif";s:4:"2f2c";s:17:"ext_localconf.php";s:4:"b40c";s:14:"ext_tables.php";s:4:"f707";s:28:"Classes/Controller/Index.php";s:4:"f96c";s:29:"Classes/Domain/Model/Flip.php";s:4:"ccfa";s:42:"Configuration/Flexform/flexform_ds_pi1.xml";s:4:"50c4";s:44:"Resources/Private/Templates/Index/Index.html";s:4:"f590";s:56:"Tests/Unit/Controller/Tx_IpIssuu_IndexControllerTest.php";s:4:"14d3";s:14:"doc/manual.sxw";s:4:"cc83";}',
 );
 
 ?>
