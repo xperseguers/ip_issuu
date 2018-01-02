ceac6c66ef9ac58f29b3725395eab7fc4bcdae59
Xavier Perseguers <xavier@causal.ch>
2018-01-02 17:43:08 +0100

    [TASK] Kickstart compatibility with TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,8 +30,8 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.1.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.0-7.1.99',
-            'typo3' => '6.2.0-7.6.99',
+            'php' => '5.5.0-7.1.99',
+            'typo3' => '7.6.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
